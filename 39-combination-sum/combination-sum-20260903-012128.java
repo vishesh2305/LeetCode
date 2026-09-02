ba33e5ecdbcaee17c43f24143135bcf2e0ecf5ac
Vishesh <visheshvasu2305@gmail.com>
2026-09-03 01:21:32 +0530

    Add solution for 39. Combination Sum (Medium) [Java]

---
+++ 39-combination-sum/combination-sum-20260903-012128.java
@@ -0,0 +1,33 @@
+// 39. Combination Sum
+// https://leetcode.com/problems/combination-sum/
+// Difficulty: Medium
+// Language:   Java
+// Submitted:  2026-09-03 01:21:28
+// Runtime:    1 ms (beats 99.97%)
+// Memory:     46 MB (beats 9.69%)
+// Topics:     Array, Backtracking
+
+class Solution {
+    List<List<Integer>> result = new ArrayList<>();
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        helper(candidates, target, 0, new ArrayList<>());
+        return result;
+    }
+
+    private void helper(int[] candidates, int target, int index, List<Integer> ds){
+        if(index == candidates.length){
+            if(target== 0){
+                result.add(new ArrayList<>(ds));
+                return;
+            }
+            return;
+        }
+
+        if(candidates[index] <= target){
+            ds.add(candidates[index]);
+            helper(candidates, target-candidates[index], index, ds);
+            ds.remove(ds.size()-1);
+        }
+        helper(candidates, target, index+1, ds);
+    }
+}
