b3c68ae58987fa02819d14acb4bb7c841cab7ab4
Vishesh <visheshvasu2305@gmail.com>
2026-09-04 00:10:11 +0530

    Add solution for 491. Non-decreasing Subsequences (Medium) [Java] (latest)

--- 491-non-decreasing-subsequences/solution.java
+++ 491-non-decreasing-subsequences/solution.java
@@ -2,39 +2,32 @@
 // https://leetcode.com/problems/non-decreasing-subsequences/
 // Difficulty: Medium
 // Language:   Java
-// Submitted:  2026-09-04 00:02:33
-// Runtime:    7 ms (beats 69.89%)
-// Memory:     51.3 MB (beats 99.91%)
+// Submitted:  2026-09-04 00:10:01
+// Runtime:    6 ms (beats 93.73%)
+// Memory:     51.8 MB (beats 98.87%)
 // Topics:     Array, Hash Table, Backtracking, Bit Manipulation
 
 class Solution {
     List<List<Integer>> result = new ArrayList<>();
-
     public List<List<Integer>> findSubsequences(int[] nums) {
         helper(nums, 0, new ArrayList<>());
         return result;
     }
 
-    private void helper(int[] nums, int index, List<Integer> current) {
-        if (current.size() >= 2) {
-            result.add(new ArrayList<>(current));
+    private void helper(int[] nums, int index, List<Integer> ds){
+        if(ds.size() >= 2){
+            result.add(new ArrayList<>(ds));
         }
-        ;
 
-        if (index == nums.length)
-            return;
+        if(index == nums.length) return;
 
         Set<Integer> seen = new HashSet<>();
-
-        for (int i = index; i < nums.length; i++) {
-            if (seen.contains(nums[i]) || (!current.isEmpty() && nums[i] < current.get(current.size() - 1)))
-                continue;
+        for(int i= index; i<nums.length; i++){
+            if(seen.contains(nums[i]) || (!ds.isEmpty() && (nums[i] < ds.get(ds.size()-1)))) continue;
             seen.add(nums[i]);
-
-            current.add(nums[i]);
-
-            helper(nums, i + 1, current);
-            current.remove(current.size() - 1);
+            ds.add(nums[i]);
+            helper(nums, i+1, ds);
+            ds.remove(ds.size()-1);
         }
 
     }
