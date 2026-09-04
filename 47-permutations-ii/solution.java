798816dd7278c0313ed70ddce5014b155b27d64f
Vishesh <visheshvasu2305@gmail.com>
2026-09-05 03:33:06 +0530

    Add solution for 47. Permutations II (Medium) [Java] (latest)

--- 47-permutations-ii/solution.java
+++ 47-permutations-ii/solution.java
@@ -2,20 +2,20 @@
 // https://leetcode.com/problems/permutations-ii/
 // Difficulty: Medium
 // Language:   Java
-// Submitted:  2026-09-05 03:29:28
+// Submitted:  2026-09-05 03:33:01
 // Runtime:    3 ms (beats 92.97%)
-// Memory:     47 MB (beats 13.13%)
+// Memory:     46.8 MB (beats 28.77%)
 // Topics:     Array, Backtracking, Sorting
 
 class Solution {
     List<List<Integer>> result = new ArrayList<>();
     public List<List<Integer>> permuteUnique(int[] nums) {
         Arrays.sort(nums);
-        boolean [] used = new boolean[nums.length];
+        boolean[] used = new boolean[nums.length];
         helper(nums, used, new ArrayList<>());
         return result;
     }
-
+    
     private void helper(int[] nums, boolean[] used, List<Integer> current){
         if(current.size() == nums.length){
             result.add(new ArrayList<>(current));
@@ -24,7 +24,9 @@ class Solution {
 
         for(int i=0; i<nums.length; i++){
             if(used[i]) continue;
-            if(i > 0 && used[i-1] == false && nums[i] == nums[i-1]) continue;
+            if(i>0 && !used[i-1] && nums[i-1] == nums[i]){
+                continue;
+            }
             current.add(nums[i]);
             used[i] = true;
             helper(nums, used, current);
