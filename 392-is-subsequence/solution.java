aca6c1023dd10709e77638bf688d7b64480a4c40
Vishesh <visheshvasu2305@gmail.com>
2026-09-05 01:46:33 +0530

    Add solution for 392. Is Subsequence (Easy) [Java] (latest)

--- 392-is-subsequence/solution.java
+++ 392-is-subsequence/solution.java
@@ -2,9 +2,9 @@
 // https://leetcode.com/problems/is-subsequence/
 // Difficulty: Easy
 // Language:   Java
-// Submitted:  2026-09-04 01:51:53
-// Runtime:    1 ms (beats 95.62%)
-// Memory:     43.9 MB (beats 5.72%)
+// Submitted:  2026-09-05 01:46:28
+// Runtime:    1 ms (beats 95.61%)
+// Memory:     43.7 MB (beats 5.69%)
 // Topics:     Two Pointers, String, Dynamic Programming
 
 class Solution {
@@ -12,13 +12,13 @@ class Solution {
         return helper(s, t, 0, 0);
     }
 
-    private boolean helper(String s, String t, int i, int j){
+    private boolean helper(String s, String t, int i , int j){
         if(i==s.length()) return true;
         if(j==t.length()) return false;
 
         if(s.charAt(i) == t.charAt(j)){
             return helper(s, t, i+1, j+1);
         }
-        return helper(s, t, i, j+1);
+        return helper(s, t , i , j+1);
     }
 }
