68118e9816fbb096efb893c61a19fc232b594377
Vishesh <visheshvasu2305@gmail.com>
2026-09-06 13:08:30 +0530

    Add solution for 60. Permutation Sequence (Hard) [Java]

---
+++ 60-permutation-sequence/permutation-sequence-20260906-130826.java
@@ -0,0 +1,39 @@
+// 60. Permutation Sequence
+// https://leetcode.com/problems/permutation-sequence/
+// Difficulty: Hard
+// Language:   Java
+// Submitted:  2026-09-06 13:08:26
+// Runtime:    1 ms (beats 99.46%)
+// Memory:     42.8 MB (beats 73.63%)
+// Topics:     Math, Recursion
+
+class Solution {
+    public String getPermutation(int n, int k) {
+
+        List<Integer> numbers = new ArrayList<>();
+
+        int fact = 1;
+
+        for(int i=1; i<n; i++){
+            fact *= i;
+            numbers.add(i);
+        }
+        numbers.add(n);
+
+        k--;
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0; i<n; i++){
+            int index = k / fact;
+            sb.append(numbers.get(index));
+            numbers.remove(index);
+            if(i==n-1) break;
+            k = k % fact;
+            fact = fact / (n-i-1);
+        }
+
+        return sb.toString();
+        
+    }
+}
