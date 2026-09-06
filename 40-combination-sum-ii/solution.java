8670efd68ba15ce27cc7511460c8378de0f83428
Vishesh <visheshvasu2305@gmail.com>
2026-09-06 13:44:52 +0530

    Add solution for 40. Combination Sum II (Medium) [Java] (latest)

--- 40-combination-sum-ii/solution.java
+++ 40-combination-sum-ii/solution.java
@@ -2,42 +2,34 @@
 // https://leetcode.com/problems/combination-sum-ii/
 // Difficulty: Medium
 // Language:   Java
-// Submitted:  2026-09-03 01:30:29
-// Runtime:    5 ms (beats 99.26%)
-// Memory:     44.5 MB (beats 99.66%)
+// Submitted:  2026-09-06 13:44:45
+// Runtime:    6 ms (beats 71.25%)
+// Memory:     45.4 MB (beats 39.16%)
 // Topics:     Array, Backtracking
 
 class Solution {
     List<List<Integer>> result = new ArrayList<>();
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
-        helper(candidates, target, 0, new ArrayList<>());
+
+        helper(candidates, 0, target, new ArrayList<>());
         return result;
     }
 
-    private void helper(int[] candidates, int target, int index, List<Integer> ds){
-        if(target==0){
+    private void helper(int[] candidates, int index,  int target, List<Integer> ds){
+        if(target == 0){
             result.add(new ArrayList<>(ds));
             return;
         }
-        if(index==candidates.length){
-            return;
-        }
-
+        if(index == candidates.length) return;
 
         for(int i= index; i<candidates.length; i++){
-            if(i > index && candidates[i] == candidates[i-1]){
-                continue;
-            }
-
-            if(candidates[i] > target){
-                break;
-            }
+            if(i > index && candidates[i] == candidates[i-1]) continue;
+            if(candidates[i] > target) break;
 
             ds.add(candidates[i]);
-            helper(candidates, target-candidates[i], i+1, ds);
+            helper(candidates, i+1, target-candidates[i], ds);
             ds.remove(ds.size()-1);
-
         }
 
     }
