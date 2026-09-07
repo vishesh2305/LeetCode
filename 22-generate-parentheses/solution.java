0ac3e0b51fecadf93ff1df0ff24d037258da07f2
Vishesh <visheshvasu2305@gmail.com>
2026-09-08 03:12:08 +0530

    Add solution for 22. Generate Parentheses (Medium) [Java] (latest)

--- 22-generate-parentheses/solution.java
+++ 22-generate-parentheses/solution.java
@@ -2,8 +2,8 @@
 // https://leetcode.com/problems/generate-parentheses/
 // Difficulty: Medium
 // Language:   Java
-// Submitted:  2026-09-08 03:01:54
-// Runtime:    1 ms (beats 85.52%)
+// Submitted:  2026-09-08 03:12:01
+// Runtime:    0 ms (beats 100.00%)
 // Memory:     44.2 MB (beats 92.74%)
 // Topics:     String, Dynamic Programming, Backtracking, Bracket Sequences
 
@@ -13,22 +13,21 @@ class Solution {
         helper(n, 0, 0, new StringBuilder());
         return result;
     }
-
-    private void helper(int n , int open, int close, StringBuilder sb){
-        if(open == n && close == n){
+    private void helper(int n , int open , int close , StringBuilder sb){
+        if(open == n && close == open){
             result.add(sb.toString());
-            return ; 
+            return;
         }
 
-        if(open < n ){
+        if(open < n){
             sb.append("(");
-            helper(n , open+1, close, sb);
-            sb.deleteCharAt(sb.length() -1);
+            helper(n, open+1, close, sb);
+            sb.deleteCharAt(sb.length() - 1);
         }
 
         if(close < open){
             sb.append(")");
-            helper(n , open, close + 1, sb);
+            helper(n, open, close + 1, sb);
             sb.deleteCharAt(sb.length() - 1);
         }
     }
