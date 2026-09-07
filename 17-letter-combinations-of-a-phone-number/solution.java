ab3c18b2df7534d9fdf2c5089ad5efeaa7dd8654
Vishesh <visheshvasu2305@gmail.com>
2026-09-08 02:38:27 +0530

    Add solution for 17. Letter Combinations of a Phone Number (Medium) [Java] (latest)

--- 17-letter-combinations-of-a-phone-number/solution.java
+++ 17-letter-combinations-of-a-phone-number/solution.java
@@ -2,43 +2,35 @@
 // https://leetcode.com/problems/letter-combinations-of-a-phone-number/
 // Difficulty: Medium
 // Language:   Java
-// Submitted:  2026-09-08 01:24:54
+// Submitted:  2026-09-08 02:38:20
 // Runtime:    0 ms (beats 100.00%)
-// Memory:     43.5 MB (beats 53.56%)
+// Memory:     43.2 MB (beats 95.02%)
 // Topics:     Hash Table, String, Backtracking
 
 class Solution {
     List<String> result = new ArrayList<>();
     public List<String> letterCombinations(String digits) {
-        String[] phoneMap = {
-            "",
-            "",
-            "abc",
-            "def",
-            "ghi",
-            "jkl",
-            "mno",
-            "pqrs",
-            "tuv",
-            "wxyz"
+
+        String[] phchars = {
+            "","",
+            "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"
         };
-
-        helper(digits, phoneMap, 0, new StringBuilder());
-
+        helper(digits, phchars, 0, new StringBuilder());
         return result;
-
     }
 
-    private void helper(String digits, String[] phoneMap, int index, StringBuilder sb){
+    private void helper(String digits, String[] phchars, int index, StringBuilder sb){
         if(index == digits.length()){
             result.add(sb.toString());
             return;
         }
+
         int digit = digits.charAt(index) - '0';
-        for(char ch : phoneMap[digit].toCharArray()){
+
+        for(char ch : phchars[digit].toCharArray()){
             sb.append(ch);
-            helper(digits, phoneMap, index+1, sb);
-            sb.deleteCharAt(sb.length()-1);
+            helper(digits, phchars, index+1, sb);
+            sb.deleteCharAt(sb.length() - 1);
         }
     }
 }
