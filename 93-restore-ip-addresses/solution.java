f06e09fbb1fbcf6702243ddbf074d4e5bd7fd7c9
Vishesh <visheshvasu2305@gmail.com>
2026-09-09 03:22:33 +0530

    Add solution for 93. Restore IP Addresses (Medium) [Java] (latest)

--- 93-restore-ip-addresses/solution.java
+++ 93-restore-ip-addresses/solution.java
@@ -2,40 +2,38 @@
 // https://leetcode.com/problems/restore-ip-addresses/
 // Difficulty: Medium
 // Language:   Java
-// Submitted:  2026-09-09 03:10:58
-// Runtime:    6 ms (beats 33.77%)
-// Memory:     46.4 MB (beats 44.81%)
+// Submitted:  2026-09-09 03:22:26
+// Runtime:    5 ms (beats 41.50%)
+// Memory:     46.6 MB (beats 43.13%)
 // Topics:     String, Backtracking
 
 class Solution {
     List<String> result = new ArrayList<>();
+
     public List<String> restoreIpAddresses(String s) {
         helper(s, 0, 0, new StringBuilder());
         return result;
     }
 
-    private void helper(String s, int Index, int segmentCount, StringBuilder sb){
-        if(segmentCount == 4 && Index == s.length()){
+    private void helper(String s, int index, int segmentcount, StringBuilder sb) {
+        if (segmentcount == 4 && index == s.length()) {
             result.add(sb.toString());
             return;
         }
 
-        for(int i=1 ; i<=3 && Index + i <= s.length() ; i++){
-            String segment = s.substring(Index, Index + i);
+        for (int i = 1; i <= 3 && index + i <= s.length(); i++) {
+            String segment = s.substring(index, index + i);
 
-            if(!(segment.length() > 1 && segment.charAt(0) == '0') && Integer.parseInt(segment) <= 255){
+            if (!(segment.length() > 1 && segment.charAt(0) == '0') && Integer.parseInt(segment) <= 255) {
+                int originalLength = sb.length();
+                if (segmentcount > 0) {
+                    sb.append(".");
+                }
+                ;
+                sb.append(segment);
 
-            int originalLength = sb.length();
-
-            if(segmentCount >0 ){
-                sb.append(".");
-            }
-
-            sb.append(segment);
-
-            helper(s, Index + i, segmentCount+1, sb);
-
-            sb.setLength(originalLength);
+                helper(s, index + i, segmentcount + 1, sb);
+                sb.setLength(originalLength);
             }
 
         }
