efb6b51afc6162ca5f32f33a39992b03093cf510
Vishesh <visheshvasu2305@gmail.com>
2026-09-09 20:39:32 +0530

    Add solution for 46. Permutations (Medium) [Java] (latest)

--- 46-permutations/solution.java
+++ 46-permutations/solution.java
@@ -2,32 +2,33 @@
 // https://leetcode.com/problems/permutations/
 // Difficulty: Medium
 // Language:   Java
-// Submitted:  2026-09-06 12:06:14
-// Runtime:    2 ms (beats 34.77%)
-// Memory:     45.6 MB (beats 45.77%)
+// Submitted:  2026-09-09 20:39:23
+// Runtime:    1 ms (beats 87.96%)
+// Memory:     45.3 MB (beats 78.29%)
 // Topics:     Array, Backtracking
 
 class Solution {
     List<List<Integer>> result = new ArrayList<>();
     public List<List<Integer>> permute(int[] nums) {
         boolean[] used = new boolean[nums.length];
-        helper(nums, used, new ArrayList<>());
+        helper(nums, 0, used, new ArrayList<>());
         return result;
     }
 
-    private void helper(int[] nums, boolean[] used, List<Integer> ds){
-        if(ds.size()== nums.length){
+    private void helper(int[] nums, int index, boolean[] used, List<Integer> ds){
+        if(ds.size()==nums.length){
             result.add(new ArrayList<>(ds));
             return;
         }
 
         for(int i=0; i<nums.length; i++){
             if(used[i]) continue;
+
             ds.add(nums[i]);
             used[i] = true;
-            helper(nums, used, ds);
+            helper(nums, i+1, used, ds);
             ds.remove(ds.size()-1);
             used[i] = false;
-        }
+        };
     }
 }
