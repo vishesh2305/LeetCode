1ad4055b3b7ccfef4e4cdb2febdd46f0baf62e54
Vishesh <visheshvasu2305@gmail.com>
2026-09-22 03:02:59 +0530

    Add solution for 79. Word Search (Medium) [Java] (latest)

--- 79-word-search/solution.java
+++ 79-word-search/solution.java
@@ -2,52 +2,46 @@
 // https://leetcode.com/problems/word-search/
 // Difficulty: Medium
 // Language:   Java
-// Submitted:  2026-09-22 02:49:11
-// Runtime:    107 ms (beats 94.53%)
-// Memory:     43.1 MB (beats 43.73%)
+// Submitted:  2026-09-22 03:02:53
+// Runtime:    104 ms (beats 94.76%)
+// Memory:     43.2 MB (beats 18.14%)
 // Topics:     Array, String, Backtracking, Depth-First Search, Matrix
 
 class Solution {
     public boolean exist(char[][] board, String word) {
-        char[] ch = word.toCharArray();
 
+        char[] ch = word.toCharArray();
 
         for(int i=0; i<board.length; i++){
             for(int j=0; j<board[0].length; j++){
-                if(helper(board, ch, i, j, 0)){
+                if(helper(board, ch, i, j , 0)){
                     return true;
                 }
             }
         }
-        return false;
 
+        return false;
+        
     }
 
-    private boolean helper(char[][] board, char[] ch,int curRow, int curCol, int index){
-        if(curRow < 0 || curRow >= board.length || curCol < 0 || curCol >= board[0].length){
-            return false;
-        }
+    private boolean helper(char[][] board, char[] ch, int curRow, int curCol, int index){
+        if(curRow < 0 || curCol < 0 || curRow >= board.length || curCol >= board[0].length) return false;
+
 
         if(board[curRow][curCol] != ch[index]) return false;
 
-        if(index == ch.length-1){
-            return true;
-        }
-
-        if(board[curRow][curCol] == ch[index]){
-
+        if(index == ch.length - 1) return true;
             char temp = board[curRow][curCol];
             board[curRow][curCol] = '#';
-
             boolean found = helper(board, ch, curRow+1, curCol, index+1) ||
             helper(board, ch, curRow-1, curCol, index+1) ||
             helper(board, ch, curRow, curCol+1, index+1) ||
             helper(board, ch, curRow, curCol-1, index+1);
 
             board[curRow][curCol] = temp;
-            return found;
-        }
 
-        return false;
+            return found;
+
+
     }
 }
